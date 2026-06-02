012e3202f8f35d3879a4bca0d341105f54c0477a
kuliahumy26-byte <kuliahumy26@gmail.com>
2026-06-02 17:49:09 +0700

    membuat input dan file

--- ioFileWithPath.cpp
+++ ioFileWithPath.cpp
@@ -8,5 +8,12 @@ int main()
     //deklarasi variabel baris dan NamaFile
     string baris, NamaFile;
 
+    //membuat input untuk menangkap nama file 
+    cout << "Masukkan nama file : ";
+    cin >> NamaFile;
+
+    //membuat file dalam mode menulis
+    ofstream outfile;
+
     
 }
