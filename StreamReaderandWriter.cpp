a2e9b69a5f3babb3049256894bba56383539f34e
kuliahumy26-byte <kuliahumy26@gmail.com>
2026-06-02 18:06:19 +0700

    membuat loop dalam menulis isi file

--- StreamReaderandWriter.cpp
+++ StreamReaderandWriter.cpp
@@ -11,10 +11,24 @@ int main()
     //membuka file dalam mode menulis
     ofstream outfile;
 
-    //mennunjuk ke sebuah nama file
+    //menunjuk ke sebuah nama file
     outfile.open("contohfile.txt");
 
     cout << ">= Menulis file, \'q\' untuk keluar " << endl;
 
+    //unlimited loop untuk menulis file
+    while (true)
+    {
+        cout << "- ";
+        //mendapat setiap karakter dalam satu baris
+        getline(cin, baris);
+        //kondisi false jika baris hanya berisi karakter q
+        if (baris == "q") break;
+        //menulis dan memasukkan nilai dari variabel baris ke dalam file
+        outfile << baris << endl;
+    }
+
     
+
+
 }
